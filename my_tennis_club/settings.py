0433028bf098925633db7a342b7d23b39a7dd5e9
anishDev419 <manandharanish419@gmail.com>
2024-05-02 15:22:56 +0545

    SASS Installed + Line Divider

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -27,9 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_sass",
     "members",
     "cbv",
     "books",
@@ -77,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "my_tennis_club.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -87,7 +83,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -107,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -119,13 +113,17 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Templates/static')]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Templates/static'),
+    os.path.join(BASE_DIR, 'static/General')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
